e37e801f113dae3024e774d83c93bbd1f2f47436
SuperChen <chenmengyang_2016@hotmail.com>
2016-08-11 02:51:18 +0800

    This is the project of QR Code

---
+++ 二维码/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace 二维码
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.选择模式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.生成模式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.识别模式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.操作方法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.二维码另存为ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.发送到手机ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.属性ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.选择模式ToolStripMenuItem,
+            this.帮助ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(449, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 选择模式ToolStripMenuItem
+            // 
+            this.选择模式ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.生成模式ToolStripMenuItem,
+            this.识别模式ToolStripMenuItem});
+            this.选择模式ToolStripMenuItem.Name = "选择模式ToolStripMenuItem";
+            this.选择模式ToolStripMenuItem.Size = new System.Drawing.Size(81, 24);
+            this.选择模式ToolStripMenuItem.Text = "选择模式";
+            // 
+            // 生成模式ToolStripMenuItem
+            // 
+            this.生成模式ToolStripMenuItem.Name = "生成模式ToolStripMenuItem";
+            this.生成模式ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.生成模式ToolStripMenuItem.Text = "生成模式";
+            this.生成模式ToolStripMenuItem.Click += new System.EventHandler(this.生成模式ToolStripMenuItem_Click);
+            // 
+            // 识别模式ToolStripMenuItem
+            // 
+            this.识别模式ToolStripMenuItem.Name = "识别模式ToolStripMenuItem";
+            this.识别模式ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.识别模式ToolStripMenuItem.Text = "识别模式";
+            this.识别模式ToolStripMenuItem.Click += new System.EventHandler(this.识别模式ToolStripMenuItem_Click);
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.操作方法ToolStripMenuItem,
+            this.关于ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            this.帮助ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.帮助ToolStripMenuItem.Text = "帮助";
+            // 
+            // 操作方法ToolStripMenuItem
+            // 
+            this.操作方法ToolStripMenuItem.Name = "操作方法ToolStripMenuItem";
+            this.操作方法ToolStripMenuItem.Size = new System.Drawing.Size(152, 24);
+            this.操作方法ToolStripMenuItem.Text = "操作方法";
+            this.操作方法ToolStripMenuItem.Click += new System.EventHandler(this.操作方法ToolStripMenuItem_Click);
+            // 
+            // 关于ToolStripMenuItem
+            // 
+            this.关于ToolStripMenuItem.Name = "关于ToolStripMenuItem";
+            this.关于ToolStripMenuItem.Size = new System.Drawing.Size(138, 24);
+            this.关于ToolStripMenuItem.Text = "关于";
+            this.关于ToolStripMenuItem.Click += new System.EventHandler(this.关于ToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 13F);
+            this.textBox1.Location = new System.Drawing.Point(58, 46);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 34);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(279, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "请选择模式";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.pictureBox1.Location = new System.Drawing.Point(82, 123);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(295, 227);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.二维码另存为ToolStripMenuItem,
+            this.发送到手机ToolStripMenuItem,
+            this.属性ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(169, 76);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // 二维码另存为ToolStripMenuItem
+            // 
+            this.二维码另存为ToolStripMenuItem.Name = "二维码另存为ToolStripMenuItem";
+            this.二维码另存为ToolStripMenuItem.Size = new System.Drawing.Size(168, 24);
+            this.二维码另存为ToolStripMenuItem.Text = "二维码另存为";
+            this.二维码另存为ToolStripMenuItem.Click += new System.EventHandler(this.二维码另存为ToolStripMenuItem_Click);
+            // 
+            // 发送到手机ToolStripMenuItem
+            // 
+            this.发送到手机ToolStripMenuItem.Name = "发送到手机ToolStripMenuItem";
+            this.发送到手机ToolStripMenuItem.Size = new System.Drawing.Size(168, 24);
+            this.发送到手机ToolStripMenuItem.Text = "发送到手机";
+            // 
+            // 属性ToolStripMenuItem
+            // 
+            this.属性ToolStripMenuItem.Name = "属性ToolStripMenuItem";
+            this.属性ToolStripMenuItem.Size = new System.Drawing.Size(168, 24);
+            this.属性ToolStripMenuItem.Text = "属性";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(449, 380);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "二维码生成/识别--陈梦杨实习";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 选择模式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 生成模式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 识别模式ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 操作方法ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 关于ToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 二维码另存为ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 发送到手机ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 属性ToolStripMenuItem;
+    }
+}
+
---
+++ 二维码/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ThoughtWorks.QRCode.Codec;
+using ThoughtWorks.QRCode.Codec.Data;
+using System.IO;
+
+namespace 二维码
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string filepath = "";
+
+        private void 关于ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("陈梦杨出品o(≧v≦)o~~好棒","关于",MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        private void 生成模式ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1.Text = "生成";
+            textBox1.Text = "";
+        }
+
+        private void 识别模式ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            button1.Text = "识别";
+            pictureBox1.Image = null;
+            textBox1.Text = "识别文本在此显示";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "生成")
+            {
+                if (textBox1.Text.Trim() != "")
+                {
+                    string enCodeString = textBox1.Text;
+                    QRCodeEncoder QRCodeEncoder = new QRCodeEncoder();
+                    pictureBox1.Image = QRCodeEncoder.Encode(enCodeString,
+                                                            Encoding.UTF8);
+                }
+                else
+                {
+                    MessageBox.Show("请输入内容：");
+                }
+            }
+            else if(button1.Text=="识别")//识别代码
+	        {
+                OpenFileDialog p = new OpenFileDialog();
+                p.Title = "请选择二维码文件";
+                p.Filter = "图片文件(*.jpg)|*.jpg";
+                p.Multiselect = false;
+                if (p.ShowDialog() == DialogResult.OK)
+                {
+                    filepath = p.FileName;
+                    System.Threading.Thread t = new System.Threading.Thread(ss);
+                    t.IsBackground = true;
+                    t.Start();
+                }
+	        }
+            else
+            {
+                MessageBox.Show("请选择模式","错误!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void ss()
+        {
+            if (filepath != "")
+            {
+                string tt = "";
+                try
+                {
+                    Invoke((EventHandler)delegate
+                    {
+                        button1.Enabled = false;
+                        button1.Text = "Waiting!";
+                        pictureBox1.Image = new Bitmap(filepath);
+                    });
+                    /*pictureBox1.Size = new Size(new Bitmap(filepath).Size.Width,
+                        new Bitmap(filepath).Size.Height);*/
+                    QRCodeDecoder qrDecoder = new QRCodeDecoder();
+                    string txtMsg = qrDecoder.decode(new QRCodeBitmapImage(
+                        new Bitmap(pictureBox1.Image)), Encoding.UTF8);
+                    tt = txtMsg;
+                }
+                catch(Exception) 
+                {
+                    tt = "识别失败";
+                }
+                Invoke((EventHandler)delegate
+                {
+                    button1.Enabled = true;
+                    textBox1.Text = tt;
+                    button1.Text = "识别";
+                });
+            }
+            System.Threading.Thread.CurrentThread.Abort();
+        }
+        private void 二维码另存为ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+                    if (pictureBox1.Image!=null)
+        {
+	        try
+	                    {
+	                        SaveFileDialog saveDlg = new SaveFileDialog();//让用户指定文件的路径名
+	                        saveDlg.Filter = "图片文件(*.jpg)|*.jpg";
+	                        if (saveDlg.ShowDialog() == DialogResult.OK)
+	                        {
+	                            //创建文件，将textBox1中的内容保存到文件中
+	                            //saveDlg.Fil   eName是用户指定的文件路径
+                                pictureBox1.Image.Save(saveDlg.FileName,
+                                    System.Drawing.Imaging.ImageFormat.Jpeg);
+	                            //提示用户文件已成功保存
+	                            MessageBox.Show("文件已成功保存到" + saveDlg.FileName);
+	                        }
+	                    }
+	                    catch (System.Exception ex)
+	                    {
+	                        //如保存不成功，用对话框提示用户
+	                        MessageBox.Show("Error!" + ex.ToString());
+	                    }
+        } 
+        else
+        {
+            MessageBox.Show("图片不存在！请生成图片后重试！");    
+        }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void 操作方法ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("打开程序，先选择模式；生成模式可以根据字符串生成二维码，识别模式可以根据系统的二维码解码为字符串。", "操作方法", MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+    }
+}
